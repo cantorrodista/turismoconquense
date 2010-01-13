4111b27897924fccb6101b7f3661c7e293ad3301
Alfredo <alfredo.solano@the-cocktail.com>
2010-01-14 00:49:59 +0100

    mas fixturas

---
+++ db/fixtures/categories.rb
@@ -0,0 +1,21 @@
+Category.seed do |s|
+  s.id = 1
+  s.name = 'Home'
+  s.nicename = 'home'
+  s.visible = 0
+end
+
+Category.seed do |s|
+  s.id = 2
+  s.name = 'Ocio'
+  s.nicename = 'ocio'
+  s.visible = 1
+end
+
+
+Category.seed do |s|
+  s.id = 3
+  s.name = 'Teatro'
+  s.nicename = 'teatro'
+  s.visible = 1
+end
